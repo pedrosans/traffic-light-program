690036c8535be59aa73cc2bc88eacfa2a6b4ff98
Pedro Santos <pedrosans@gmail.com>
2011-10-03 12:51:41 -0300

    Nova abstração

--- src/test/java/tcc/ambiente/CromossomoTest.java
+++ src/test/java/tcc/ambiente/CromossomoTest.java
@@ -27,16 +27,16 @@ import tcc.AmbienteTeste1;
 
 public class CromossomoTest {
 
-	@Test
-	public void test() {
-		Cromossomo cromossomo = new Cromossomo();
-		AmbienteTeste1 ambienteTeste1 = new AmbienteTeste1();
-		cromossomo.genotipos = Arrays.asList(new Genotipo[] {//
-				new Genotipo(0, 0), new Genotipo(0, 0), new Genotipo(0, 0) //
-				});
-		cromossomo.calculaIndeceAdaptabilidade(ambienteTeste1);
-		
-		assertTrue(cromossomo.indiceAdaptabilidade != 0);
-	}
+    @Test
+    public void test() {
+        Cromossomo cromossomo = new Cromossomo();
+        AmbienteTeste1 ambienteTeste1 = new AmbienteTeste1();
+        cromossomo.genotipos = Arrays.asList(new Genotipo[] {//
+                new Genotipo(0, 0), new Genotipo(0, 0), new Genotipo(0, 0) //
+                });
+        cromossomo.calculaIndeceAdaptabilidade(ambienteTeste1);
+
+        assertTrue(cromossomo.indiceAdaptabilidade != 0);
+    }
 
 }
